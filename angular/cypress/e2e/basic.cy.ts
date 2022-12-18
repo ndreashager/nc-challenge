3e0308ca8c891455617c97aa5c74d19edc2eed6c
Andreas Häger <andreas@hager.ax>
2022-12-18 19:46:03 +0200

    Removed e2e test and updated .gitignore

--- angular/cypress/e2e/basic.cy.ts
+++ angular/cypress/e2e/basic.cy.ts
@@ -3,15 +3,7 @@ describe('empty spec', () => {
     cy.visit('/')
   })
 
-  it('displays the resources text', () => {
-    cy.get('h2')
-    .contains('This is a bare-bones Angular project that has everything you need to quickly deploy it to Netlify');
-  })
-  it('renders the image', () => {
-    cy.get('img')
-    .should('be.visible')
-    .and(($img) => {
-      expect($img[0].naturalWidth).to.be.greaterThan(0);
-    })
+  it('Does not do much!', () => {
+    expect(true).to.equal(true)
   })
 })
